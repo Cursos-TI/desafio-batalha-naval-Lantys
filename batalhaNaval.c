c1748847e968960ad30f9738113f846354c1234c
Lantys <lantysu@gmail.com>
2025-11-01 00:18:42 -0300

    batalhaNaval.c
    Criação do desafio da batalha naval - Novato

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,110 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <string.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
+// Desafio Batalha naval
+// Tema 1: Geração do tabuleiro
 
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
-
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
-
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
-
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
+	
+	setlocale(LC_ALL, "Portuguese");
+    int index;
+    int coluna;
+    int linha;
+    int con = 10;
+    int lin = 10;
+    int tiro_max = 10;
+    int tiro = tiro_max;;
     
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
-
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
-
+    // Matriz de validação dos navios x acertos
+    char * tabuleiro1 [10] [10] = {
+	{"3", "3", "3", "0", "0", "0", "0", "0", "0", "0"},
+	{"0", "0", "0", "0", "0", "0", "0", "0", "0", "0"},
+	{"0", "0", "0", "0", "0", "0", "0", "0", "0", "0"},
+	{"0", "0", "0", "0", "0", "0", "0", "0", "0", "0"},
+	{"0", "0", "3", "0", "0", "0", "0", "0", "0", "0"},
+	{"0", "0", "3", "0", "0", "0", "0", "0", "0", "0"},
+	{"0", "0", "3", "0", "0", "0", "0", "0", "0", "0"},
+	{"0", "0", "0", "0", "0", "0", "0", "0", "0", "0"},
+	{"0", "0", "0", "0", "0", "0", "0", "0", "0", "0"},
+	{"0", "0", "0", "0", "0", "0", "0", "0", "0", "0"},
+	};
+	
+	// Cria o tabuleiro a ser impresso no cmd
+	char * tabuleiro2 [10] [10] = {
+	{"0", "0", "0", "0", "0", "0", "0", "0", "0", "0"},
+	{"0", "0", "0", "0", "0", "0", "0", "0", "0", "0"},
+	{"0", "0", "0", "0", "0", "0", "0", "0", "0", "0"},
+	{"0", "0", "0", "0", "0", "0", "0", "0", "0", "0"},
+	{"0", "0", "0", "0", "0", "0", "0", "0", "0", "0"},
+	{"0", "0", "0", "0", "0", "0", "0", "0", "0", "0"},
+	{"0", "0", "0", "0", "0", "0", "0", "0", "0", "0"},
+	{"0", "0", "0", "0", "0", "0", "0", "0", "0", "0"},
+	{"0", "0", "0", "0", "0", "0", "0", "0", "0", "0"},
+	{"0", "0", "0", "0", "0", "0", "0", "0", "0", "0"},
+	};
+	
+	// Printa o menu inicial 
+	system("cls");
+	printf("\n ===========================================================");
+    printf("\n \t\t Batalha Naval");
+    printf("\n ===========================================================\n\n");
+	 
+	//Repetição da estrutura dos execução dos tiros
+	while (tiro > 0 ){
+		
+		system("cls");
+		printf("\n ===========================================================");
+	    printf("\n \t\t Batalha Naval");
+	    printf("\n ===========================================================\n");
+	    
+		// Print de acertos após o primeiro loop completo.
+		if (tiro != tiro_max) {
+			if (tabuleiro1[linha][coluna] == "3"){
+				printf("\n-------------------------------------------------------------");
+				printf("\n \t Você acertou o tiro");
+				printf("\n-------------------------------------------------------------\n");
+				tabuleiro2 [linha][coluna] = "3";
+			}else{
+				printf("\n-------------------------------------------------------------");
+				printf("\n \t Você errou o tiro \n");
+				printf("\n-------------------------------------------------------------\n");
+			}   	
+		}
+		
+		// Print do tabuleiro a cada loop
+		for (int i = 0; i < lin; i++) {
+			printf("\t");
+	        for (int j = 0; j < con; j++) {
+	            printf(" %s ", tabuleiro2[i][j]); 
+	        }
+	        printf("\n");
+		}
+		printf("\n - Você ainda tem %d  tiros", tiro);
+		printf("\n-------------------------------------------------------------");
+		
+		//Mostra o menu do jogo
+		printf("\n - Selecione de 0 - 9 para a linha do tiro %d \n", tiro);
+		scanf("%d", &linha); // Recebe a linha da matrix
+		fflush(stdin);
+		printf(" -------------------------------------------------------------");
+		printf("\n - Selecione de 0 - 9 para a Coluna do tiro %d \n", tiro);
+		scanf("%d", &coluna); // Recebe a coluna da matrix
+		fflush(stdin);		
+		
+		// Popula o acerto no tabuleiro que é impresso no loop.
+		if (tabuleiro1[linha][coluna] == "3"){
+			printf("\n \t Você acertou o tiro \n");
+			tabuleiro2 [linha][coluna] = "3";
+		}else{
+			printf("\n \t Você errou o tiro \n");
+		}
+	
+		// Redução da quantidade de tiros restantes.
+	    tiro--;
+  		
+	}
+    
     return 0;
 }
